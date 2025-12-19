ce7ce370f2f5b51d789faec518b6d042b6cd4444
theaarondumas <aardumas@gmail.com>
2025-12-19 05:51:03 -0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "wvt_cache_v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
